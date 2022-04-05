8234c9ad67f83f9e62b9867e04000f934950dd9f
Vitor Balbino de Castro <vitor.balbino.gd@gmail.com>
2022-04-05 13:02:21 -0300

    Steering behaviour

--- Assets/_OurAssets/CarAgent.cs
+++ Assets/_OurAssets/CarAgent.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarAgent : MonoBehaviour
-{
+public class CarAgent : MonoBehaviour {
     [Header("Movement")]
     public float MaxSpeed = 10.0f;
-    public float CurrentSpeed = 30.0f;
+    public float MaxAceleration = 10.0f;
+    public Vector3 CurrentSpeed = Vector3.zero;
+    public float Mass = 1.0f;
 
     [Header("Raycast")]
     public Vector3 OriginPoint;
@@ -22,20 +23,21 @@ public class CarAgent : MonoBehaviour
     RaycastHitHandler HitHandler = new RaycastHitHandler();
 
 
-    
+
     void Update()// Rodando uma vez para testes
     {
+        OriginPoint = this.transform.position + (this.transform.forward * 1.5f) + (Vector3.up * 0.5f);
         CenterOfOpenArea = Vector3.zero;
         pointCount = 0;
 
         // Para cada raio lançado no espaço
-        for (int rayIndex = 0; rayIndex < RayQuantitie; rayIndex++) {
+        for(int rayIndex = 0; rayIndex < RayQuantitie; rayIndex++) {
             // Um ângulo é atribuído (0 a 180)
             float angle = rayIndex * 180 / (RayQuantitie - 1);
             float angleInRad = angle * Mathf.PI / (float)180.0;
 
             // Uma direção é atribuída baseada no ângulo
-            Vector3 direction = Vector3.forward * Mathf.Sin(angleInRad) + Vector3.right * Mathf.Cos(angleInRad);
+            Vector3 direction = this.transform.forward * Mathf.Sin(angleInRad) + this.transform.right * Mathf.Cos(angleInRad);
 
             // Um raycast é lançado para cada ângulo
             HitHandler.Ray(direction, this);
@@ -59,14 +61,15 @@ public class CarAgent : MonoBehaviour
          */
 
 
-        // 
+        // Atualizar posição
+
+        MovementHandler.Seek(this);
     }
 }
 
 
 
-public class RaycastHitHandler
-{
+public class RaycastHitHandler {
     public RaycastHit Hit;
 
     Color BeforeHitColor = Color.yellow;
@@ -75,12 +78,12 @@ public class RaycastHitHandler
     Color TooCloseHitColor = Color.red;
 
     public void Ray(Vector3 DirectionVector, CarAgent car) {
-        Vector3 OriginPoint = car.OriginPoint + car.transform.position;
+        Vector3 OriginPoint = car.OriginPoint;
 
         if(Physics.Raycast(OriginPoint, DirectionVector, out Hit, car.MaxRayDistance, car.DefaultRaycastLayers)) {
-                        
+
             Debug.DrawRay(OriginPoint, DirectionVector * Hit.distance, Hit.distance > car.TooCloseDistance ? BeforeHitColor : TooCloseHitColor);
-            Debug.DrawRay(Hit.point, DirectionVector * (car.MaxRayDistance - Hit.distance) , AfterHitColor);
+            Debug.DrawRay(Hit.point, DirectionVector * (car.MaxRayDistance - Hit.distance), AfterHitColor);
 
             car.CenterOfOpenArea += Hit.point;
             car.pointCount += 1;
@@ -94,4 +97,38 @@ public class RaycastHitHandler
         }
     }
 
+}
+
+
+
+public static class MovementHandler {
+    public static Vector3 TruncateInMagnitude(this Vector3 value, float Limit) {
+        return (value.magnitude > Limit ? value.normalized * Limit : value);
+    }
+
+    public static Vector3 GetHeight(this Vector3 value, MonoBehaviour Reference) {
+        value.y = Reference.transform.position.y;
+
+        return value;
+    }
+
+    public static void Seek(CarAgent car) {
+        Vector3 desired_vel = (car.CenterOfOpenArea.GetHeight(car) - car.transform.position).normalized * car.MaxSpeed;
+
+        Vector3 steeringForce = desired_vel - car.CurrentSpeed;
+
+        steeringForce = TruncateInMagnitude(steeringForce, car.MaxAceleration);
+
+        Vector3 acelleration = steeringForce / car.Mass;
+
+        car.CurrentSpeed = TruncateInMagnitude(car.CurrentSpeed + acelleration, car.MaxSpeed);
+
+        Vector3 desiredPosition = car.transform.position + car.CurrentSpeed * Time.deltaTime;
+
+        car.transform.LookAt(desiredPosition);
+
+        car.transform.position = desiredPosition;
+
+        Debug.Log($"CurrentSpeed = {car.CurrentSpeed};  acelleration = { acelleration}; desiredPosition = {desiredPosition}");
+    }
 }
